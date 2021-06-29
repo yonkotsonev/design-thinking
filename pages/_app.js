a560aa38d4715e6b35870cc64c053a31d36093cd
Yonko Tsonev <y.tsonev@adcash.com>
2021-06-29 15:52:12 +0300

    Added main feature

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.css'
+
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,97 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React, { useState } from 'react';
 
 export default function Home() {
+  const [stickies, setStickies] = useState([]);
+  var current = null;
+
+  const showModal = () =>  {
+    const sticky = document.getElementById('sticky');
+    sticky.style.display = 'block';
+    document.getElementById("sticky-content").focus();
+
+    if (current !== null) {
+      document.getElementById("sticky-content").value = stickies[current];
+    }
+  };
+
+  const closeModal = () => {
+    document.getElementById("sticky-content").value = null;
+
+    const sticky = document.getElementById('sticky');
+    sticky.style.display = 'none';
+  }
+
+  const saveSticky = () => {
+    var newStickies = [...stickies];
+    var value = document.getElementById("sticky-content").value;
+    if (!value) {
+      closeModal();
+      return;
+    }
+
+    if (current != null) {
+      newStickies[current] = value;
+      current = null;
+    } else {
+      newStickies.push(value);
+    }
+
+    setStickies(newStickies);
+
+    closeModal();
+  };
+
+  const deleteSticky = () => {
+    if (current != null ) {
+      var newStickies = [...stickies];
+      newStickies.splice(current, 1);
+      
+      current = null;
+
+      setStickies(newStickies);
+    }
+
+    closeModal();
+  }
+
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Design Thinking App</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <main className="border border-1 border-dark">
+       <div className="border border-1 border-dark buttons btn-group p-3 w-100">
+        <button className="btn btn-dark mx-1" onClick={showModal}>Create Sticky</button>
+        <button className="btn btn-danger mx-1" onClick={() => setStickies([])}>Clear Board</button>
+       </div>
+       <div className="my-2 w-100 h-90">
+          <div id="content">
+            {stickies.map((value, index) => {
+              return <div id={index} key={index} onClick={() => {current = index; showModal()}} className="bg-warning sticky m-1 p-1 border border-2 border-dark">{value}</div>
+            })}
+          </div>
+       </div>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+       <div id="sticky" className="p-5">
+          <div className="w-100 h-100">
+            <div className="text-center w-100 h-90">
+              <textarea id="sticky-content" className="w-100 h-100 bg-warning border border-dark border-1" maxLength="800">
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+              </textarea>
+            </div>
+            <div className="btn-group w-100 p-3">
+              <button className="btn btn-dark mx-1" onClick={saveSticky}>OK</button>
+              <button className="btn btn-danger mx-1" onClick={deleteSticky}>DELETE</button>
+            </div>
+          </div>
+       </div>
+       <div id="background">
+        <p id="bg-text">Yonko's Design Thinking App</p>
+      </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+    </>
   )
 }
